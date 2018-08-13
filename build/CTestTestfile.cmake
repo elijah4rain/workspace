d7233949c8447f0e31bb5a7de1a74d3f4ee87bf4
Elijah S. Lee <elijahsjlee@gmail.com>
2018-08-13 14:06:58 -0400

    commit ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/elijah/catkin_ws/src
+# Build directory: /home/elijah/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("video_radiation_noise")
---
+++ build/video_radiation_noise/CMakeFiles/radiation_image_overlay.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radiation_image_overlay.dir/src/nodes/radiation_image_overlay.cpp.o"
+  "/home/elijah/catkin_ws/devel/lib/video_radiation_noise/radiation_image_overlay.pdb"
+  "/home/elijah/catkin_ws/devel/lib/video_radiation_noise/radiation_image_overlay"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/radiation_image_overlay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
